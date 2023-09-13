af2ff8835d665e98fe72b258441c8825ec4e4bc4
Nneji123 <ifeanyinneji777@gmail.com>
2023-09-13 04:59:54 +0100

    reformatted instructions

--- automate.py
+++ automate.py
@@ -30,14 +30,14 @@ if __name__ == "__main__":
     title_text = pyfiglet.figlet_format("OamFuture", font="slant")
     print(title_text)
     script_info = """
-    [bold cyan]Automation Script Information[/bold cyan]
-    [bold green]Purpose:[/bold green] This script automates the process of signing up for an account on OamFuture website.
-    [bold green]Functionality:[/bold green] It generates random numbers, fills out a signup form with these numbers,
-    and handles proxies for anonymity.
-    [bold yellow]Instructions:[/bold yellow]
-    1. Make sure you have Chrome browser installed.
-    2. Download Chromedriver from 'https://chromedriver.chromium.org/downloads' and place it in C:\\chromedriver\\chromedriver.exe.
-    3. Run the script and follow the prompts.
+[bold cyan]Automation Script Information[/bold cyan]
+[bold green]Purpose:[/bold green] This script automates the process of signing up for an account on OamFuture website.
+[bold green]Functionality:[/bold green] It generates random numbers, fills out a signup form with these numbers,
+and handles proxies for anonymity.
+[bold yellow]Instructions:[/bold yellow]
+1. Make sure you have Chrome browser installed.
+2. Download Chromedriver from 'https://chromedriver.chromium.org/downloads' and place it in C:\\chromedriver\\chromedriver.exe.
+3. Run the script and follow the prompts.
     """
     rprint(script_info)
     check_chrome_and_chromedriver()
